d2dfc1dc1ddcca8d25f60d051b0c8464b2c222bf
ClementMonard <clement.monard@hotmail.fr>
2018-06-22 11:54:47 +0200

    Ajout des commentaires dans le script

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+//Déclaration de la fonction
 function keyboardTyping(){
+  //Déclaration de la variable pour obtenir sa valeur
   var lastname = document.getElementById('lastname').value;
+  //affichage de la variable
   alert(lastname);
 }
